fc351fc75f7203cf5553ba78ceb3395ea65c4133
axel paulander <axel.paulander@gmail.com>
2024-10-07 11:59:55 +0200

    Add GIF as separate media type

--- src/media.rs
+++ src/media.rs
@@ -4,6 +4,7 @@ use serde::Serialize;
 pub enum MediaType {
     Photo,
     Video,
+    Gif,
 }
 
 impl MediaType {
@@ -11,11 +12,15 @@ impl MediaType {
         match self {
             Self::Photo => {
                 //Missing either CreateDate or DatetimeOriginal
-                "((not $Exif:CreateDate or not $Exif:DatetimeOriginal) and $MIMEType=~/image/)"
+                "((not $Exif:CreateDate or not $Exif:DatetimeOriginal) and $MIMEType=~/image/ and not $FileType eq \"GIF\")"
             }
             Self::Video => {
                 //Missing CreateDate in both groups
                 "(not $QuickTime:CreateDate and not $Xmp:CreateDate and $MIMEType=~/video/)"
+            }
+            Self::Gif => {
+                //Like a photo but with a different tag group
+                "((not $Xmp:CreateDate or not $Xmp:DatetimeOriginal) and $MIMEType=~/image/ and $FileType eq \"GIF\")"
             }
         }
     }
